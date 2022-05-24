6d79cbdc0ec70da4b95427be65604dc42b8650a5
Stephen H <74723672+StephenHughesUni@users.noreply.github.com>
2022-05-24 12:28:17 +0100

    Add files via upload

---
+++ Semester One/Study week 2/Lab week 2/week2exercises.py
@@ -0,0 +1,63 @@
+# Writen by : Stephen Hughes, 12/10/2020, 1A(2) -  This is a lab - Week 2 -  to show how to use print functions
+
+# Question 1  - Name and address on different
+print()
+print("Stephen Hughes - X00179579")
+print("260 Clonard Road Crumlin")
+print()
+
+# Question 2 -  Python Code as seen on Lab Exercise
+print("...............................")
+print(". Software Development 1      .")
+print(". Python Programming Language .")
+print("...............................")
+
+# Question 3 - Square and Triangle
+print()
+
+print("*************\n*\t\t\t*\n*\t\t\t*\n*************")
+
+print()
+
+print("  /\ ")
+print(" /  \\")
+print("/____\\")
+print()
+
+# Question 4 - Sum Calculations
+print()
+
+print("5 + 5 = ", 5+5)
+print("5 * 5 = ", 5*5)
+
+print()
+# Question 5 - Add the text
+print("The Answer is : 10")
+print("The Answer is : 25")
+
+print()
+
+# Question 6 - Calculating result with print statement
+print("The answer to 5 x 2 is :", 5*2)
+
+# Question 7 - Add comments to program
+
+# Question 8 - Generate an error.
+# primt("Game Over")
+#  Line 50 -   primt("Game Over")
+# NameError: name 'primt' is not defined.
+
+# Question 9 - Here it is using two print statements.
+print("Here it", end="")
+print(" is")
+print()
+# Question 10 Produce same output as photo on sheet.
+print("   Credits\n \\ \\ \\ \\ \\ \\ \\\n     by\n Guido Rossum\n\\ \\ \\ \\ \\ \\ \\ \\")
+print()
+
+# Question 11 Produce same output as photo on sheet.
+print("If an adventurer returns from a successful quest and buys each of\nhis 6 companions 3 cups of coffee, how many cups were purchased?")
+
+# Question 12 - Write print to calculate above.
+print()
+print("The ammount of coffee purchased would be:", 6*3)
